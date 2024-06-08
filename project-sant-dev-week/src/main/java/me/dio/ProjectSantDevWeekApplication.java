9d0cea1600a5a8644483ffd334fc75e586fe97bd
Belisnalva Costa <bella-costa@hotmail.com.br>
2024-06-07 22:59:11 -0300

    Initial commit

---
+++ project-sant-dev-week/src/main/java/me/dio/ProjectSantDevWeekApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSantDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSantDevWeekApplication.class, args);
+	}
+
+}
